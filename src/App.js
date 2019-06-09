1ee5e8b9d310a1a7748df42a9a3d83e84f4d0762
Gabe Pierannunzi <gabethecoder@icloud.com>
2019-06-09 16:04:27 -0600

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import React, { useState } from 'react';
+
+import './App.css';
+import logo from './logo.svg'
+
+const App = props => {
+
+    const [result, setResult] = useState(null);
+
+    return (
+        <div className="App">
+            <img src={logo} className="App-logo" alt="logo" />
+
+            {result}
+            <button onClick={() => setResult('test')}>
+                update
+            </button>
+        </div>
+    );
+
+};
 
 export default App;
